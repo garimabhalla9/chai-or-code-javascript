ff11b351855e68da8ee088696dde461783911fdc
Garima Bhalla <149891439+garimabhalla9@users.noreply.github.com>
2024-10-16 16:28:06 +0000

    commit

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,8 +23,68 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 let someNumber=33
 
 let stringNumber= String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************ Operations *********************
+
+let value =3
+let negValue= -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " garima"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);           //12
+// console.log(1+ "2");            //12
+// console.log("1" + 2 + 2);       //122
+// console.log(1 + 2 + "2");       //32
+
+// console.log((3+ 4) * 5 % 3);
+
+
+//Not to be used
+// console.log(+true);     //1
+// console.log(+"");       //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2         //not to be used
+
+let gameCounter = 100
+// gameCounter++;                 //101
+++gameCounter;                    //101
+console.log(gameCounter);         
+
+
+//link to study operations
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+//link to study increment/decrement operations (prefix/postfix)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
